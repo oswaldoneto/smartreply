8d4edb59033246adabc4a62b3ec857d794175d89
oswaldo.neto <oswaldo.neto@gmail.com>
2017-05-17 18:29:20 -0300

    implementacao do pipeline de tratamento dos dados

--- ml/shortcuts.py
+++ ml/shortcuts.py
@@ -1,8 +1,11 @@
 import os
 import json
+import re
 
 from sklearn.ensemble.forest import RandomForestClassifier
 from sklearn.feature_extraction.text import CountVectorizer
+from nltk.corpus import stopwords
+from unicodedata import normalize
 
 from smartreply.settings import BASE_DIR
 
@@ -22,7 +25,7 @@ def predict(text):
     clean_train_complain = []
     target_problem_type = []
     for complain in data:
-        clean_train_complain.append(complain['title'])
+        clean_train_complain.append(clean_data(complain['complain']))
         target_problem_type.append(complain['category'])
 
     vectorizer = CountVectorizer(analyzer="word",
@@ -44,7 +47,7 @@ def predict(text):
 
     clean_test_complain = []
 
-    clean_test_complain.append(text)
+    clean_test_complain.append(clean_data(text))
 
     test_data_features = vectorizer.transform(clean_test_complain)
     test_data_features = test_data_features.toarray()
@@ -52,5 +55,81 @@ def predict(text):
     result = forest.predict(test_data_features)
 
     return result
+
+
+def clean_data(pure_text):
+
+
+    def clean_properties(pure_text):
+        """
+        Remove things like 'RA: NA123-10' 'E-mail: bla@email.com'          
+        """
+        clean_data = re.sub('([\wZáéíóúÁÉÍÓÚâêîôÂÊÎÔãõÃÕçÇ-]+)(?:\:\s).+', ' ', pure_text)
+        return clean_data
+
+    def clean_email(pure_text):
+        """
+        Remove things like 'bla@email.com'          
+        """
+        clean_data = re.sub(r'[\w\.-]+@[\w\.-]+', ' ', pure_text)
+        return clean_data
+
+    def clean_pontuation(pure_text):
+        """
+        Remove things like '. : , -'          
+        """
+        clean_data = re.sub(u'[^a-zA-ZáéíóúÁÉÍÓÚâêîôÂÊÎÔãõÃÕçÇ: ]', ' ', pure_text)
+        return clean_data
+
+    def clean_words_with_number(pure_text):
+        """
+        Remove things like 'N278A9'          
+        """
+        clean_data = re.sub(r'\w*\d\w*',' ', pure_text )
+        return clean_data
+
+    def clean_lower(pure_text):
+        return pure_text.lower()
+
+    def clean_single_white_space(pure_text):
+        return ' '.join(pure_text.split())
+
+    def clean_stop_words(pure_text):
+        stops = stopwords.words('portuguese')
+        meaningful_words = [word for word in pure_text.split() if not word in stops]
+        return ' '.join(meaningful_words)
+
+    def clean_greeting_words(pure_text):
+        greeting_words = ['ola', 'olá', 'oi', 'bom-dia', 'boa-tarde', 'boa-noite', 'boa', 'noite', 'tarde', 'dia']
+        meaningful_words = [word for word in pure_text.split() if not word in greeting_words]
+        return ' '.join(meaningful_words)
+
+    def clean_acentos(pure_text):
+        return normalize('NFKD', pure_text).encode('ASCII', 'ignore').decode('ASCII')
+
+
+    clean_data = clean_properties(pure_text)
+
+    clean_data = clean_words_with_number(clean_data)
+
+    clean_data = clean_email(clean_data)
+
+    clean_data = clean_pontuation(clean_data)
+
+    clean_data = clean_lower(clean_data)
+
+    clean_data = clean_single_white_space(clean_data)
+
+    clean_data = clean_stop_words(clean_data)
+
+    clean_data = clean_greeting_words(clean_data)
+
+    clean_data = clean_acentos(clean_data)
+
+    return clean_data
+
+
+
+
 
 
